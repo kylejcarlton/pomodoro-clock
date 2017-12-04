ab427770b29e8debc18e9394dc3bba459d756fb9
Kyle J Carlton <kylejcarlton@gmail.com>
2017-12-03 18:44:28 -0600

    begin logic for work timer

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+  var workTime = 1500000, breakTime = 300000, workTimeLeft, breakTimeLeft, minutes, seconds;
 
-}); //END DOC READY JS
+  workTimeLeft = workTime;
+  setInterval(workingTime, 1000);
+  function workingTime(){
+    workTimeLeft = workTimeLeft - 1000;
+    minutes = Math.floor((workTimeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((workTimeLeft % (1000 * 60)) / 1000);
+    $("#wt").html(minutes+":"+seconds);
+
+    if(workTimeLeft < 0){
+      clearInterval();
+    }
+  }
+
+}); //END DOC READY
